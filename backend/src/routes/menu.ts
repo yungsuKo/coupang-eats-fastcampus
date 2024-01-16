de2d670bfaf5c604e7f397b4e4d15966ff3e947c
Tom Ko <yungsu2391@gmail.com>
2024-01-16 09:48:04 +0900

    package 내 start 추가, menu 조회 추가

--- backend/src/routes/menu.ts
+++ backend/src/routes/menu.ts
@@ -1,9 +1,57 @@
+import { Menu } from '@/db/models/menu';
+import { Store } from '@/db/models/store';
 import express from 'express';
+import { ObjectId } from 'mongodb';
 
 const router = express.Router();
 
-router.get('/:menuId', async (req, res) => {});
+/**
+ * 메뉴의 정보를 갖고온다
+ *
+ * @param menuId
+ */
+router.get('/:menuId', async (req, res) => {
+  const { menuId } = req.params;
+  try {
+    const doc = await Menu.findById(menuId);
+    res.status(200).json(doc);
+  } catch (err) {
+    console.error('getting menu error', err);
+    res.sendStatus(500);
+  }
+});
 
-router.get('/store/:storeId', async (req, res) => {});
+/**
+ * 특정 매장의 메뉴를 가져온다
+ *
+ * @param storeId
+ */
+router.get('/store/:storeId', async (req, res) => {
+  const { storeId } = req.params;
+  try {
+    const docs = await Store.aggregate([
+      {
+        $match: { _id: new ObjectId(storeId) },
+      },
+      {
+        $unwind: '$menus',
+      },
+      {
+        $lookup: {
+          from: 'menus',
+          localField: 'menus',
+          foreignField: '_id',
+          as: 'menu',
+        },
+      },
+    ]);
+    console.log(docs);
+
+    res.status(200).json(docs);
+  } catch (err) {
+    console.error('store lookup failed');
+    res.sendStatus(500);
+  }
+});
 
 export default router;
