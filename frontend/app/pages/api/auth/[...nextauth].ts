13ec2609e0ba8e27e44d6844e469cb1f77978197
Tom Ko <yungsu2391@gmail.com>
2024-01-20 10:40:10 +0900

    next-auth 세팅

---
+++ frontend/app/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import { MongoClient } from 'mongodb'
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import clientPromise from 'app/lib/mongoClient'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+}
+
+export default NextAuth(authOptions)
