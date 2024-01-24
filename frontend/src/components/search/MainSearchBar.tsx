a557a1501e6e77eeecba39038fe1f5d26cf38571
Tom Ko <yungsu2391@gmail.com>
2024-01-24 10:23:45 +0900

    문자

--- frontend/src/components/search/MainSearchBar.tsx
+++ frontend/src/components/search/MainSearchBar.tsx
@@ -1,11 +1,20 @@
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { FaSearch } from 'react-icons/fa';
 
 export const MainSearchBar = () => {
+  const { data } = useSession();
+  console.log(data);
   return (
     <div>
-      <Link href="/search">
+      <Link
+        className="flex items-center justify-stretch rounded-full border"
+        href="/search"
+      >
         <FaSearch />
+        {data?.user?.name
+          ? `${data?.user?.name}님 맥도날드 어떠심?`
+          : '검색해보세요'}
       </Link>
     </div>
   );
