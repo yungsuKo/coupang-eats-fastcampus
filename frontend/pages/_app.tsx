96ff1d861e9d92bf80514d71f2975bbcf0aecb3b
Tom Ko <yungsu2391@gmail.com>
2024-01-25 10:03:41 +0900

    tailwind 적용

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { AuthRedirect } from '../src/components/common/AuthRedirect';
+import '@/src/app/globals.css';
 
 export default function App({
   Component,
