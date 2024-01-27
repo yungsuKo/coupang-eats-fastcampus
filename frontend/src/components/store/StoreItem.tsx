3f52fc2aace14ce92f0aef22dea0df1a2e808c18
Tom Ko <yungsu2391@gmail.com>
2024-01-27 14:34:41 +0900

    store component 생성

---
+++ frontend/src/components/store/StoreItem.tsx
@@ -0,0 +1,5 @@
+import { Store } from '@/pages/types/store';
+
+export const StoreItem = ({ store }: { store: Store }) => {
+  return <div></div>;
+};
--- frontend/src/components/store/StoreList.tsx
+++ frontend/src/components/store/StoreList.tsx
@@ -1,3 +1,18 @@
+import { useStores } from '@/pages/queries/stores';
+import { StoreFilter } from './StoreFilter';
+import { StoreItem } from './StoreItem';
+
 export const StoreList = () => {
-  return <div></div>;
+  const { isLoading, error, data } = useStores();
+
+  if (isLoading || error || !data) return null;
+
+  return (
+    <div>
+      <StoreFilter />
+      {data.pages.map((page) =>
+        page.map((store) => <StoreItem key={store._id} store={store} />)
+      )}
+    </div>
+  );
 };
