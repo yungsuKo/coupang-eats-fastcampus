347f3883d69975c45157d8ca32bf49c69ab882f2
Tom Ko <yungsu2391@gmail.com>
2024-01-30 09:45:08 +0900

    infinite scroll 구현

--- frontend/src/components/store/StoreList.tsx
+++ frontend/src/components/store/StoreList.tsx
@@ -1,10 +1,13 @@
 import { useStores } from '@/pages/queries/stores';
 import { StoreFilters } from './StoreFilters';
 import { StoreItem } from './StoreItem';
+import { useInfiniteScroll } from '@/src/lib/useInfiniteScroll';
 
 export const StoreList = () => {
-  const { isLoading, error, data } = useStores();
+  const { isLoading, error, data, hasNextPage, isFetching, fetchNextPage } =
+    useStores();
 
+  const loader = useInfiniteScroll(hasNextPage, isFetching, fetchNextPage);
   if (isLoading || error || !data) return null;
 
   return (
@@ -13,6 +16,7 @@ export const StoreList = () => {
       {data.pages.map((page) =>
         page.map((store) => <StoreItem key={store._id} store={store} />)
       )}
+      {!isFetching && <div ref={loader} />}
     </div>
   );
 };
---
+++ frontend/src/lib/useInfiniteScroll.ts
@@ -0,0 +1,34 @@
+import {
+  FetchNextPageOptions,
+  InfiniteQueryObserverResult,
+} from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
+
+export function useInfiniteScroll<TData, TScroll>(
+  hasNextPage: boolean | undefined,
+  isFetching: boolean,
+  fetchNextPage: (
+    options?: FetchNextPageOptions
+  ) => Promise<InfiniteQueryObserverResult<TData[], TError>>
+) {
+  const loader = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const options: IntersectionObserverInit = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      const target = entries[0];
+      if (target.isIntersecting && hasNextPage && !isFetching) {
+        fetchNextPage();
+      }
+    }, options);
+
+    if (loader.current) observer.observe(loader.current);
+  }, [fetchNextPage, hasNextPage, isFetching]);
+
+  return loader;
+}
