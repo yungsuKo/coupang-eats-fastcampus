4a5ae5a72cd68289a3f7d11056f808053f744c7a
Tom Ko <yungsu2391@gmail.com>
2024-02-08 09:41:30 +0900

    order type 정의

---
+++ frontend/pages/types/order.ts
@@ -0,0 +1,24 @@
+import { DeliveryStatus } from '@/src/constants/deliveryStatus';
+import { Menu } from './menu';
+import { Store } from './store';
+
+export type OrderHistory = {
+  _id: string;
+  timestamp: string;
+  status: DeliveryStatus;
+  user: string;
+  menus: OrderMenu[];
+  store: Store;
+  review: string;
+};
+
+export type OrderMenu = {
+  menu: Menu;
+  count: number;
+  options: [
+    {
+      title: string;
+      price: number;
+    }
+  ];
+};
