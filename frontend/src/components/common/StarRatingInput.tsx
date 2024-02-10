36f0b2dc439311e4ff89caee706d36d835cba79c
Tom Ko <yungsu2391@gmail.com>
2024-02-10 20:09:46 +0900

    review 작성 페이지 component 구현

---
+++ frontend/src/components/common/StarRatingInput.tsx
@@ -0,0 +1,21 @@
+import { SetStateAction } from 'jotai';
+import { Dispatch } from 'react';
+import { FaRegStar, FaStar } from 'react-icons/fa';
+
+export const StarRatingInput = ({
+  rating = 5,
+  setRating,
+}: {
+  rating: number;
+  setRating: Dispatch<SetStateAction<number>>;
+}) => {
+  return (
+    <div className="flex">
+      {[...Array(5)].map((_, i) => (
+        <button onClick={() => setRating(i + 1)} key={i}>
+          {i < rating ? <FaStar /> : <FaRegStar />}
+        </button>
+      ))}
+    </div>
+  );
+};
